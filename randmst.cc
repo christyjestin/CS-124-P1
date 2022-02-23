c58c25f640f356cbb14361059828121c612cd132
Christy Jestin <cjj9000@gmail.com>
2022-02-23 02:19:16 -0500

    implement fully; no pruning

--- randmst.cc
+++ randmst.cc
@@ -1,7 +1,6 @@
 #include <vector>
 #include <algorithm>
-#include <cstdlib> 
-#include <ctime>
+#include <cstdlib>
 #include <numeric>
 #include <cassert>
 #include <math.h>
@@ -13,27 +12,29 @@ struct edge {
     float dist;
 };
 
-// node struct, has name and coords
 struct euclid_node {
     int v;
     std::vector<float> coords;
+
+    euclid_node(int i, int dim) {
+        v = i;
+        coords = std::vector<float> (dim, 0);
+    }
 };
 
 // random number between 0 to 1
 float unif() {
-    // reseed each time
-    srand(std::time(0));
-    return ((float) std::rand() / RAND_MAX);
+    return ((float) rand() / RAND_MAX);
 }
 
 float dist(std::vector<float> a, std::vector<float> b) {
     assert(a.size() == b.size());
     std::vector<float> diff;
-    for (int i = 0; i < a.size(); i++) {
-        int d = a[i] - b[i];
+    for (int i = 0; i < (int) a.size(); i++) {
+        float d = a[i] - b[i];
         diff.push_back(d * d);
     }
-    return sqrt(std::accumulate(diff.begin(), diff.end(), 0));
+    return sqrt(std::accumulate(diff.begin(), diff.end(), 0.0f));
 }
 
 std::vector<edge> zero_dim_graph(int n) {
@@ -50,9 +51,10 @@ std::vector<edge> higher_dim_graph(int n, int dim) {
     std::vector<euclid_node> nodes;
     std::vector<edge> edges;
     for (int i = 0; i < n; i++) {
-        euclid_node new_node;
-        new_node.v = i;
-        std::fill(new_node.coords.begin(), new_node.coords.begin(), unif());
+        euclid_node new_node (i, dim);
+        for (auto it = new_node.coords.begin(); it != new_node.coords.end(); it++) {
+            *it = unif();
+        }
         nodes.push_back(new_node);
     }
     for (int i = 0; i < n; i++) {
@@ -76,21 +78,29 @@ std::vector<edge> kruskal(int n, std::vector<edge> edges) {
         if (sets.find(edge.u) != sets.find(edge.v)) {
             sets.unite(edge.u, edge.v);
             mst.push_back(edge);
+            if ((int) mst.size() == n-1) break;
         }
     }
     return mst;
 }
 
-int main() {
-    int n = 4;
-    std::vector<edge> graph = zero_dim_graph(n);
-    std::vector<edge> mst = kruskal(n, graph);
-    printf("graph");
-    for (auto edge : graph) {
-        printf("i: %d, j: %d, dist: %f", edge.u, edge.v, edge.dist);
-    }
-    printf("mst");
-    for (auto edge : mst) {
-        printf("i: %d, j: %d, dist: %f", edge.u, edge.v, edge.dist);
+inline float add_weight(float sum, edge e) {
+    return sum + e.dist;
+}
+
+float sum_weights(std::vector<edge> mst) {
+    return std::accumulate(mst.begin(), mst.end(), 0.0f, add_weight);
+}
+
+int main(int argc, char** argv) {
+    assert(argc == 5);
+    int n = atoi(argv[2]);
+    int numtrials = atoi(argv[3]);
+    int dim = atoi(argv[4]);
+    srand(time(0));
+    for (int i = 0; i < numtrials; i++) {
+        std::vector<edge> graph = dim == 0 ? zero_dim_graph(n) : higher_dim_graph(n, dim);
+        std::vector<edge> mst = kruskal(n, graph);
+        printf("%f\n", sum_weights(mst));
     }
 }
