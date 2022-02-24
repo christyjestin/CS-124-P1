c82c8b80e23043553318c492f1f709e21cb6aa3a
Ash Ahmed <ashlanahmed@college.harvard.edu>
2022-02-24 04:11:22 -0500

    working w/ fixed cutoffs pruned

---
+++ max_weights.cc
@@ -0,0 +1,2 @@
+// placeholder for dim = 1
+float weights[5] = {0.001257, 0, 0.026732, 0.074125, 0.159176};
--- randmst.cc
+++ randmst.cc
@@ -5,6 +5,7 @@
 #include <cassert>
 #include <math.h>
 #include "ufds.cc"
+#include "max_weights.cc"
 
 struct edge {
     int u;
@@ -27,6 +28,7 @@ float unif() {
     return ((float) rand() / RAND_MAX);
 }
 
+// finds distance between 2 points `a` and `b` in n-dimensional space
 float dist(std::vector<float> a, std::vector<float> b) {
     assert(a.size() == b.size());
     std::vector<float> diff;
@@ -41,13 +43,17 @@ std::vector<edge> zero_dim_graph(int n) {
     std::vector<edge> edges;
     for (int i = 0; i < n; i++) {
         for (int j = i+1; j < n; j++) {
-            edges.push_back(edge {i, j, unif()});
+            float weight = unif();
+            if (n <= 8192 || weight < 2 * weights[0]) {
+                edges.push_back(edge {i, j, weight});
+            }
         }
     }
     return edges;
 }
 
 std::vector<edge> higher_dim_graph(int n, int dim) {
+    assert(dim != 1 && dim < 5);
     std::vector<euclid_node> nodes;
     std::vector<edge> edges;
     for (int i = 0; i < n; i++) {
@@ -59,14 +65,17 @@ std::vector<edge> higher_dim_graph(int n, int dim) {
     }
     for (int i = 0; i < n; i++) {
         for (int j = i+1; j < n; j++) {
-            edges.push_back(edge {i, j, dist(nodes[i].coords, nodes[j].coords)});
+            float weight = dist(nodes[i].coords, nodes[j].coords);
+            if (n <= 8192 || weight < 2 * weights[dim]) {
+                edges.push_back(edge {i, j, weight});
+            }
         }
     }
     return edges;
 }
 
 std::vector<edge> kruskal(int n, std::vector<edge> edges) {
-    ufds sets;
+    ufds sets (n);
     for (int i = 0; i < n; i++) {
         sets.makeset(i);
     }
@@ -74,22 +83,35 @@ std::vector<edge> kruskal(int n, std::vector<edge> edges) {
 
     std::vector<edge> mst;
 
-    for (auto edge : edges) {
-        if (sets.find(edge.u) != sets.find(edge.v)) {
-            sets.unite(edge.u, edge.v);
-            mst.push_back(edge);
+    for (edge e : edges) {
+        if (sets.find(e.u) != sets.find(e.v)) {
+            sets.unite(e.u, e.v);
+            mst.push_back(e);
             if ((int) mst.size() == n-1) break;
         }
     }
     return mst;
 }
 
+// helper function for sum_weights
 inline float add_weight(float sum, edge e) {
     return sum + e.dist;
 }
 
+// sum the weights of all edges in a minimum spanning tree
 float sum_weights(std::vector<edge> mst) {
     return std::accumulate(mst.begin(), mst.end(), 0.0f, add_weight);
+}
+
+// find largest weight of any edge in a minimum spanning tree
+float largest_weight(std::vector<edge> mst) {
+    sort(mst.begin(), mst.end(), [](const auto &x, const auto &y){return x.dist < y.dist;});
+    return mst.back().dist;
+}
+
+// find the average weight of edges in a minimum spanning tree
+float avg(std::vector<float> sums) {
+    return (std::accumulate(sums.begin(), sums.end(), 0.0f)) / sums.size();
 }
 
 int main(int argc, char** argv) {
@@ -98,9 +120,51 @@ int main(int argc, char** argv) {
     int numtrials = atoi(argv[3]);
     int dim = atoi(argv[4]);
     srand(time(0));
+
+    float max_weight;
+    std::vector<float> tree_sizes;
     for (int i = 0; i < numtrials; i++) {
         std::vector<edge> graph = dim == 0 ? zero_dim_graph(n) : higher_dim_graph(n, dim);
         std::vector<edge> mst = kruskal(n, graph);
-        printf("%f\n", sum_weights(mst));
+
+        float size = sum_weights(mst);
+        tree_sizes.push_back(size);
+
+
+        float lw = largest_weight(mst);
+        printf("largest weights: %f\n", lw);
+        if (lw > max_weight) max_weight = lw;
+    
     }
+    printf("max largest weight: %f\n", max_weight);
+    printf("average tree size: %f\n", avg(tree_sizes));
 }
+
+// n=128, 5 trials, 2D: 0.15450
+// n=256, 5 trials, 2D: 0.105021
+// n=512, 5 trials, 2D: 0.085148
+// n=1024, 5 trials, 2D: 0.063156
+// n=2048, 5 trials, 2D: 0.040723
+// n=4096, 5 trials, 2D: 0.030502
+// n=8192, 5 trials, 2D: 0.026732
+
+//0.10502
+
+
+// n = 8192, 0.001257 max largest for 0D
+// n = 8192, d = 3d, get 0.074125
+// n = 8192, d = 4d, get 0.159176
+
+
+
+
+
+
+// RESULTS - numtrials = 5 for everything
+// n = 128; 256; 512; 1024; 2048; 4096; 8192; 16384; 32768; 65536; 131072; 262144;
+
+// D = 0: [1.151236, 1.231376, ]
+
+// to do: try shell scripting to automate these commands for diff inputs
+// - run like 20 trials for 8192 and make that the cutoff
+// - christy idea: do same cutoff idea but use the 8192 cutoff for 16384 trials, use those for 131072, etc. - do this for all dims
